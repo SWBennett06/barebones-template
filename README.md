503cf5db2377ceac396bdd0036e0a24f1a9bdd5b
Sean Bennett <swbennett06@gmail.com>
2018-02-21 18:44:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Barebones Template
+
+A barebones Webpack build that serves as a launchpad for random experiments and prototyping.
+
+Out of the Box: 
+
+* TS w/HMR
+* SCSS
+* Jest
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: '[name].[contenthash].css',
+});
+
+module.exports = {
+  entry: ['./src/index.ts', './src/main.scss'],
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: './dist',
+      compress: true,
+      open: true,
+      hot: true
+  },
+  plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HTMLWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      extractSass
+  ],
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+        rules: [
+            { test: /\.html$/, use: ['html-loader']},
+            { test: /\.tsx?$/, use: ['ts-loader']},
+            { test: /\.(sass|scss)$/, use: extractSass.extract({
+                use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'sass-loader'
+                    }
+                ], fallback: 'style-loader' })
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
