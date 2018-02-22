3e1d96bfcf5f7ef4512319c3c498bc6372bdffde
Sean Bennett <swbennett06@gmail.com>
2018-02-21 19:33:11 -0600

    FEAT: Added UglifyJS plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
@@ -9,28 +10,29 @@ const extractSass = new ExtractTextPlugin({
 });
 
 module.exports = {
-  entry: ['./src/index.ts', './src/main.scss'],
-  devtool: 'inline-source-map',
-  devServer: {
-      contentBase: './dist',
-      compress: true,
-      open: true,
-      hot: true
-  },
-  plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HTMLWebpackPlugin({
-        template: './src/index.html'
-      }),
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      extractSass
-  ],
-  output: {
-    filename: '[name].[hash].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
+    entry: ['./src/index.ts', './src/main.scss'],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        open: true,
+        hot: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        extractSass,
+        new UglifyJSPlugin()
+    ],
+    output: {
+        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
         rules: [
             { test: /\.html$/, use: ['html-loader']},
             { test: /\.tsx?$/, use: ['ts-loader']},
